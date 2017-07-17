013a27fc8de62ef82498cb3296b5ceb8d86ff382
andrewpapale <papalae04@gmail.com>
2017-07-17 15:57:10 -0400

    update

--- getTrailComponents.m
+++ getTrailComponents.m
@@ -69,66 +69,61 @@ for iS=1:nSegments
     okflag = 0;
     while ~okflag
         fprintf('Click on line segments \n');
-        [xC2,yC2] = ginput(2);
+        [xC2,yC2] = ginput(1);
         if ~isempty(xC)
             % find closest endpoint to click
-            P1 = plot(xC2(1),yC2(1),'gx','markersize',10); hold on;
-            P2 = plot(xC2(1),yC2(1),'go','markersize',10); hold on;
-            P3 = plot(xC2(2),yC2(2),'gx','markersize',10); hold on;
-            P4 = plot(xC2(2),yC2(2),'go','markersize',10); hold on;
+            P1 = plot(xC2,yC2,'gx','markersize',10); hold on;
+            P2 = plot(xC2,yC2,'go','markersize',10); hold on;
+            P3 = plot(xC,yC,'gx','markersize',10); hold on;
+            P4 = plot(xC,yC,'go','markersize',10); hold on;
         else
             disp('Try again');
         end
         
-        m = (yC2(2) - yC2(1)) / (xC2(2) - xC2(1));
-        if m<1 && m>0
-            m=0.1;
-        elseif m>-1 && m<0
-            m=-0.1;
-        end
-        mp = -1/m;
-        result = computeline([xC2(1),yC2(1)],mp,[xC2(1)-boxSize xC2(1)+boxSize]);
-        for iR=1:length(result)
-            result1(iR,1:2) = result{iR};
-        end
-        result = computeline([xC2(2),yC2(2)],mp,[xC2(2)-boxSize xC2(2)+boxSize]);
-        for iR=1:length(result)
-            result2(iR,1:2) = result{iR};
-        end
-        for iR=1:length(result)
-            line{iR} = linepts([result1(iR,1),result1(iR,2)],[result2(iR,1),result2(iR,2)]);
-        end
-        tempX = [];
-        tempY = [];
-        for iR=1:length(result)
-            templine = line{iR};
-            nL=length(templine);
-            for iL=1:nL
-                tempX = cat(1,tempX,templine{iL}(1,1));
-                tempY = cat(1,tempY,templine{iL}(1,2));
-            end
-        end
-        tempsegment = zeros(size(data));
-        k = tempX<=1 | tempY<=1 | tempX>size(data,2) | tempY>size(data,1) | isnan(tempX) | isnan(tempY);
-        tempX(k)=[];
-        tempY(k)=[];
-        for iL=1:length(tempX)
-            tempsegment(round(tempY(iL)),round(tempX(iL)))=1;
-        end
-        se = strel('line',11,90);
-        tempsegment = imdilate(tempsegment,se);
-        tempsegment = bwmorph(tempsegment,'majority');
-        [yS0,xS0]=find(tempsegment==1);
-        [ySI0,xSI0] = find(data==1 & tempsegment==1);
-        
-        keyboard;
-        
-        P5 = plot(xS0,yS0,'gx');
-        P6 = plot(xSI0,ySI0,'r.');
+%         m = (yC2 - yC) / (xC2 - xC);
+%         mp = -1/m;
+%         result = computeline([xC2,yC2],mp,[xC2-boxSize xC2+boxSize]);
+%         for iR=1:length(result)
+%             result1(iR,1:2) = result{iR};
+%         end
+%         result = computeline([xC,yC],mp,[xC-boxSize xC+boxSize]);
+%         for iR=1:length(result)
+%             result2(iR,1:2) = result{iR};
+%         end
+%         for iR=1:length(result)
+%             line{iR} = linepts([result1(iR,1),result1(iR,2)],[result2(iR,1),result2(iR,2)]);
+%         end
+%         tempX = [];
+%         tempY = [];
+%         for iR=1:length(result)
+%             templine = line{iR};
+%             nL=length(templine);
+%             for iL=1:nL
+%                 tempX = cat(1,tempX,templine{iL}(1,1));
+%                 tempY = cat(1,tempY,templine{iL}(1,2));
+%             end
+%         end
+%         tempsegment = zeros(size(data));
+%         k = tempX<=1 | tempY<=1 | tempX>size(data,2) | tempY>size(data,1) | isnan(tempX) | isnan(tempY) | tempY > yC+boxSize | tempY < yC-boxSize;
+%         tempX(k)=[];
+%         tempY(k)=[];
+%         for iL=1:length(tempX)
+%             tempsegment(round(tempY(iL)),round(tempX(iL)))=1;
+%         end
+%         se = strel('line',11,90);
+%         tempsegment = imdilate(tempsegment,se);
+%         tempsegment = bwmorph(tempsegment,'majority');
+%         %[yS0,xS0]=find(tempsegment==1);
+%         [ySI0,xSI0] = find(data==1 & tempsegment==1);
+%      
+%         keyboard;
+%         
+%         %P5 = plot(xS0,yS0,'gx');
+%         P6 = plot(xSI0,ySI0,'r.');
         
         % compute vector (pointing towards connecting endpoint)
-        x1 = xC-xC2;
-        y1 = yC-yC2;
+        x1 = xC2-xC;
+        y1 = yC2-yC;
         mag = sqrt(x1.^2+y1.^2);
         
         
@@ -139,16 +134,11 @@ for iS=1:nSegments
             if strcmp(okStr, 'y');
                 okflag = 1;
                 flag3 = 1;
-                xS{iS} = xS0;
-                yS{iS} = yS0;
-                segment{iS} = tempsegment;
-                xSI{iS} = xSI0;
-                ySI{iS} = ySI0;
                 vec.x0{iS} = x1;
                 vec.y0{iS} = y1;
                 vec.mag{iS} = mag;
-                xC0{iS} = xC;
-                yC0{iS} = yC;
+                xC0{iS} = xC2;
+                yC0{iS} = yC2;
             elseif strcmp(okStr, 'n');
                 okflag = 0;
                 flag3 = 1;
@@ -156,8 +146,8 @@ for iS=1:nSegments
                 delete(P2);
                 delete(P3);
                 delete(P4);
-                delete(P5);
-                delete(P6);
+%                 delete(P5);
+%                 delete(P6);
             else
                 flag3 = 0;
             end
@@ -168,7 +158,8 @@ end
 
 tempStr = strsplit(handles.FileName,'-');
 saveStr = strcat(tempStr{1},'-',tempStr{2},'-',tempStr{3},'-',tempStr{4},'-',tempStr{5},'-','ConnectingPoint.mat');
-save(saveStr,'xC','yC','outside','inside','xS','yS','segment','xSI','ySI','vec','xC0','yC0');
+disp(saveStr);
+save(saveStr,'xC','yC','outside','inside','vec','xC0','yC0');
 
 
 end
